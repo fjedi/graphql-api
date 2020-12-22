d5df9caf18239f1519d3b8732383f0d077738079
Alexander Radyushin <r.alex.andr@ya.ru>
2020-12-22 14:08:00 +0300

    Minor fix

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -3,13 +3,13 @@ import isoWeek from 'dayjs/plugin/isoWeek';
 
 dayjs.extend(isoWeek);
 
-export type TimeConstructor = typeof dayjs;
+export type TimeConstructor = typeof dayjs & Dayjs;
 export type DateValue = Date | Dayjs | string | number;
 
 export function formatDate(date: DateValue, format = 'D MMMM YYYY, HH:mm'): string {
   return dayjs(date).format(format);
 }
 
-export const time = dayjs;
+export const time = dayjs as TimeConstructor;
 
 export default dayjs;
