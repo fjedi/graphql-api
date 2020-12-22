d53b38e2f9432f455615a0ae5a17da4b5fc51155
Alexander Radyushin <r.alex.andr@ya.ru>
2020-12-22 13:38:10 +0300

    Added isoWeek plugin for Dayjs

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -1,4 +1,7 @@
 import dayjs, { Dayjs } from 'dayjs';
+import isoWeek from 'dayjs/plugin/isoWeek';
+
+dayjs.extend(isoWeek);
 
 export function formatDate(date: Date | Dayjs | string, format = 'D MMMM YYYY, HH:mm'): string {
   return dayjs(date).format(format);
